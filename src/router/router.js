8f6ad18fce3cce4d8b74642e7efb3920604d7dd9
hechao <buerchao@163.com>
2020-06-02 14:57:56 +0800

    动态加载左侧子菜单，以及刷新页面后，头部菜单激活不发生变化

--- src/router/router.js
+++ src/router/router.js
@@ -75,16 +75,19 @@ function loadSubMenu(to) {
   let path = to.path;
   //默认跳转根路由，激活头部第一个主菜单
   let subMenu = store.getters.menus[0];
-  if(path !=='/'){
+  if (path ==='/'){
+    store.commit('SET_SUBMENU',subMenu);
+    store.commit('SET_ACTIVEINDEX',subMenu.path)
+  }else {
     //不是根路由，则截取url第一节后匹配出对应的子菜单
     let index = path.substr(1).indexOf('/');
     if (index!==-1){
       path = path.substr(0,index+1);
     }
     subMenu = store.getters.menus.filter(menu => menu.path === path);
+    store.commit('SET_SUBMENU',subMenu[0]);
+    store.commit('SET_ACTIVEINDEX',subMenu[0].path)
   }
-  store.commit('SET_SUBMENU',subMenu[0])
-  store.commit('SET_ACTIVEINDEX',subMenu[0].path)
 }
 
 /**
