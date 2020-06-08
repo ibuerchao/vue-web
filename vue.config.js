11a078d2bff8c19792467903b94ae457258c302b
hechao <buerchao@163.com>
2020-06-08 13:56:07 +0800

    vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,50 @@
+'use strict'
+const path = require('path')
+const settings = require('./src/settings.js')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+const name = settings.title // 网址标题
+const port = 8110 // 端口配置
+
+// All configuration item explanations can be find in https://cli.vuejs.org/config/
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+  devServer: {
+    port: port,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8111',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/api'
+        }
+      }
+    }
+  },
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src'),
+      }
+    }
+  },
+  transpileDependencies: [
+    'vue-echarts',
+    'resize-detector'
+  ]
+}
